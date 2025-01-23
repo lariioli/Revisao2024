cb4524465bb7a1a4f2aae742865fb1d02d9a0380
Larissa de Oliveira <larissaoliveira3zb@gmail.com>
2025-01-22 22:14:14 -0300

    Adicionando cores ao código

--- GeradordeSenhas/Program.cs
+++ GeradordeSenhas/Program.cs
@@ -5,7 +5,9 @@ class Program
 {
     static void Main()
     {
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("=== Gerador de Senhas ===");
+        Console.ResetColor();
 
         Console.Write("Informe o tamanho da Senha: ");
         int tamanho = int.Parse(Console.ReadLine());
@@ -16,17 +18,4 @@ class Program
         Console.Write("Incluir caracteres especiais? (s/n): ");
         bool incluirCaracteres = Console.ReadLine().ToLower() == "s";
 
-        string senha = GerarSenha( tamanho, incluirLetras, incluirCaracteres);
-        Console.WriteLine("A senha gerada é : " + senha);
-
-        SalvarSenha(senha);
-        Console.WriteLine("Senha salva no arquivo no arquivo 'bkp.TXT'.");
-
-        Console.WriteLine("Deseja ver as senhas salvas? (s/n): ");
-
-        if(Console.ReadLine().ToLower() == "s")
-        {
-            RecuperarSenhas();
-        }
-    }
-}
+        string senha = GerarSenha(tamanho, incluirLetras, incluirCaracteres);
