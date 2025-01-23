171116dc416cff989d7f0d8084354b0870e8c97c
Larissa de Oliveira <larissaoliveira3zb@gmail.com>
2025-01-22 21:37:57 -0300

    Finalizando a parte de código que gera a senha

--- GeradordeSenhas/Program.cs
+++ GeradordeSenhas/Program.cs
@@ -5,18 +5,19 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine( "=== Gerador de Senhas ===");
+        Console.WriteLine("=== Gerador de Senhas ===");
 
-        Console.Write( "Informe o tamanho da Senha: ");
+        Console.Write("Informe o tamanho da Senha: ");
         int tamanho = int.Parse(Consolole.ReadLine());
 
         Console.Write( "Incluir letras? (s/n): ");
         bool incluirLetras = Console.ReadLine().ToLower() == "s";
 
-        Console.Write( "Incluir caracteres especiais? (s/n): ");
+        Console.Write("Incluir caracteres especiais? (s/n): ");
         bool incluirCaracteres = Console.ReadLine().ToLower() == "s";
 
-
+        string senha = GerarSenha( tamanho, incluirLetras, incluirCaracteres);
+         Console.WriteLine("A senha gerada é : " + senha);
 
 
     }
