da5fd503795b95accc1c3bd7ef56735d6d96f302
Larissa de Oliveira <larissaoliveira3zb@gmail.com>
2025-01-22 21:54:15 -0300

    Adicionando ao código a opção de salvar em TXT

--- GeradordeSenhas/Program.cs
+++ GeradordeSenhas/Program.cs
@@ -8,17 +8,25 @@ class Program
         Console.WriteLine("=== Gerador de Senhas ===");
 
         Console.Write("Informe o tamanho da Senha: ");
-        int tamanho = int.Parse(Consolole.ReadLine());
+        int tamanho = int.Parse(Console.ReadLine());
 
-        Console.Write( "Incluir letras? (s/n): ");
+        Console.Write("Incluir letras? (s/n): ");
         bool incluirLetras = Console.ReadLine().ToLower() == "s";
 
         Console.Write("Incluir caracteres especiais? (s/n): ");
         bool incluirCaracteres = Console.ReadLine().ToLower() == "s";
 
         string senha = GerarSenha( tamanho, incluirLetras, incluirCaracteres);
-         Console.WriteLine("A senha gerada é : " + senha);
+        Console.WriteLine("A senha gerada é : " + senha);
 
+        SalvarSenha(senha);
+        Console.WriteLine("Senha salva no arquivo no arquivo 'bkp.TXT'.");
 
+        Console.WriteLine("Deseja ver as senhas salvas? (s/n): ");
+
+        if(Console.ReadLine().ToLower() == "s")
+        {
+            RecuperarSenhas();
+        }
     }
 }
